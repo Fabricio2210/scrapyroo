1453d46f1367df468e63018830a1460c78301213
Fabricio <41438768+Fabricio2210@users.noreply.github.com>
2019-10-02 18:34:22 -0300

    Update SocketIoConexaoFrontEnd.js

--- socket_backend/io/SocketIoConexaoFrontEnd.js
+++ socket_backend/io/SocketIoConexaoFrontEnd.js
@@ -1,81 +1,75 @@
-const phantom = require('phantom');
-const cheerio = require('cheerio');
 const url = require('url');
 const request = require('request');
 const path = require('path');
 const fs = require('fs');
 const mongoose = require('mongoose');
 const shortid = require('shortid');
+const puppeteer = require('puppeteer');
 require('../bd/Schemas/imgUrlSchema');
 
 const ImgModelo = mongoose.model('urlSchema')
- let _ft, _pag;
  module.exports = function(io){
     io.sockets.on('connection',(socket)=>{
         console.log(`Usuário ${socket.id} conectou`);
         socket.on('teste',(data)=>{
-         phantom.create().then((ph)=>{
-             _ft = ph;
-             return _ft.createPage();
-         }).then((page)=>{
-             _pag = page;
-             return _pag.open(data.urlToScrap);
-         }).then((status)=>{
-             //console.log(status);
-             return _pag.property('content')
-         }).then((content)=>{
-             html = content;
-             const $ = cheerio.load(html);
-             const imagens = $('img');
-             let imagensPathLista = [];
-             let imagensPathServidorLista =[];
-             let hostScrap = url.parse(data.urlToScrap,true)
-             let hostScrapFullPath = `${hostScrap.protocol}//${hostScrap.host}`;
-             imagens.each((index,el)=>{
-                 if($(el).attr('src')=== undefined){
-                     socket.emit('img',"erro")
-                 }else if($(el).attr('src').charAt(0) == 'h' ){
-                    imagensPathLista.push($(el).attr('src'));
-                 }else if($(el).attr('src').charAt(1) == '/'){
-                    imagensPathLista.push(`${hostScrap.protocol}${$(el).attr('src')}`);
-                 }else{
-                    imagensPathLista.push(`${hostScrapFullPath}${$(el).attr('src')}`)
-                 }; 
-             });
-             const dir = `public/${hostScrap.host}_${shortid.generate()}`
-             if (!fs.existsSync(dir)){
-                 fs.mkdirSync(dir);
-             };
-             imagensPathLista.forEach((imgUrl,index)=>{
-                 const fileExten = path.extname(imgUrl);
-                 request(imgUrl).pipe(fs.createWriteStream(`${dir}/${index}${fileExten}`).on('error',erro=>{
-                     console.log(erro)
-                 }));
-                 process.on('uncaughtException', err => {
-                    //socket.emit('img',"erro");
-                  });
-                 let pathImg = path.relative(process.cwd(), `./${dir}/${index}${fileExten}`).split('public')
-                 imagensPathServidorLista.push(`http://localhost:3000${pathImg[1]}`);  
-             });
-             let modeloImg = new ImgModelo({
-                 urlSite: data.urlToScrap,
-                 imgPath: imagensPathServidorLista
-             });
-             modeloImg.save()
-             .then(()=>{}); 
-             _pag.close()
-             _ft.exit().then(()=>{
-                ImgModelo.findOne({urlSite:data.urlToScrap})
-                .then((data)=>{
-                   socket.emit('img',data.imgPath)
-                })
-                //console.log(socket.id)
-            });
-         }).catch((e)=>{
-         console.log(e); 
-         });  
-          
+            (async () => {
+                try {
+                    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+                  const page = await browser.newPage();
+                  await page.goto(data.urlToScrap);
+              
+                const imgs = await page.$$eval('img[src]', imgs => imgs.map(img => img.getAttribute('src')));
+                 
+                let imagensPathLista = [];
+                let imagensPathServidorLista =[];
+                const hostScrap = url.parse(data.urlToScrap,true)
+                const hostScrapFullPath = `${hostScrap.protocol}//${hostScrap.host}`;
+                if(imgs.length === 0){
+                    socket.emit('img',"erro") 
+                }else{
+                    imgs.forEach((imgUrl)=>{
+                        if(imgUrl.charAt(0) === 'h'){
+                            imagensPathLista.push(imgUrl);
+                        }else if(imgUrl.charAt(1) === '/'){
+                            imagensPathLista.push(`${hostScrap.protocol}${imgUrl}`);
+                        }else{
+                            imagensPathLista.push(`${hostScrapFullPath}${imgUrl}`);
+                        } 
+                    });
+                };
+                const pathId = shortid.generate();
+                const dir = `public/${hostScrap.host}_${pathId}`;
+                if (!fs.existsSync(dir)){
+                    fs.mkdirSync(dir,{ recursive: true });
+                   };
+                imagensPathLista.forEach((imgUrl,index)=>{
+                    const fileExten = path.extname(imgUrl);
+                        request(imgUrl).pipe(fs.createWriteStream(`${dir}/${index}${fileExten}`).on('error',erro=>{
+                            console.log(erro)
+                        }));
+                        process.on('uncaughtException', err => {
+                            return err
+                        });
+                        const pathImg = path.relative(process.cwd(), `./${dir}/${index}${fileExten}`).split('public')
+                            imagensPathServidorLista.push(`https://scrappyroo.herokuapp.com${pathImg[1]}`);  
+                        });
+                        const modeloImg = new ImgModelo({
+                            urlSite: data.urlToScrap,
+                            imgPath: imagensPathServidorLista,
+                            pathId: pathId
+                        });
+                        modeloImg.save()
+                        .then(()=>{
+                            ImgModelo.findOne({pathId})
+                            .then((data)=>{
+                                socket.emit('img',data.imgPath)
+                            })
+                        });
+                  await browser.close();
+                } catch (error) {
+                  console.log(error);
+                }
+            })();
         });
-        console.log(socket.rooms) 
       });
     };
